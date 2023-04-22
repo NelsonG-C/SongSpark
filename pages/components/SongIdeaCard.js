798fb5ebdb6083bbe613613558fffca8fc55e05c
NelsonG-C <nelsongc59@gmail.com>
2023-04-22 17:33:42 +1000

    update song card design to have most basic card design

--- pages/components/SongIdeaCard.js
+++ pages/components/SongIdeaCard.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import sparks from "../../assets/sparks.png";
 const TextArrayComponent = ({ text }) => {
   // Split the text into an array at "\n"
   const textArray = text.split("\n");
@@ -13,9 +15,25 @@ const TextArrayComponent = ({ text }) => {
 const SongIdeaCard = ({ text }) => {
   return (
     <div>
+      <a
+        href="#"
+        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <Image
+          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src={sparks}
+          alt=""
+        />
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Your New Song Inspiration
+          </h5>
+          <TextArrayComponent text={text}></TextArrayComponent>
+        </div>
+      </a>
+
       <div className="output flex justify-center">
         <div className="output-header-container"></div>
-        <TextArrayComponent text={text}></TextArrayComponent>
       </div>
     </div>
   );
