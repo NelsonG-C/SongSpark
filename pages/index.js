52f19a3c9274004b97046ed02a24b48819387656
NelsonG-C <nelsongc59@gmail.com>
2023-04-27 18:45:00 +1000

    update loading spinner when button clicked

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import SpotifyRecommendationCard from "../components/SpotifyRecommendationCard";
 import Navbar from "../components/ui/Navbar";
 import SearchBar from "../components/ui/SearchBar";
 import Background from "../components/Background";
+import LoadingSpinner from "../components/ui/LoadingSpinner";
 
 const RootApp = () => {
   return (
@@ -194,7 +195,7 @@ const Home = () => {
               >
                 <div className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 md:mr-4">
                   {isGenerating ? (
-                    <span className="loader"></span>
+                    <LoadingSpinner />
                   ) : (
                     <p>A random spark of inspiration</p>
                   )}
